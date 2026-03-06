06dda14cade908602c41fa3143df709ba0f74ff9
Bhargaviyadav1011 <23eg105g43@anurag.edu.in>
2026-03-06 18:05:36 +0530

    Fix unused role variable preventing Vercel build

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { useParams, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import AdminDashboard from './AdminDashboard';
 import ManagerDashboard from './ManagerDashboard';
 import UserDashboard from './UserDashboard';
 
 function Dashboard() {
-  const { role } = useParams();
-  
   // Extract trailing part of dashboard/* using location
   const pathParts = window.location.pathname.split('/');
   const activeRole = pathParts[pathParts.length - 1];
